efa799ab002c3edb240cc118282ebb6822491021
Jeremie Parker <jparker@guimiesoft.com>
2014-09-10 10:37:44 +0200

    changed version to 0.1.0 - updated readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-TODO: write something meaningful here
+Base Cipher package for [CryptoJS](https://code.google.com/p/crypto-js/), JavaScript implementations of standard
+and secure cryptographic algorithms.
+
+More at https://code.google.com/p/crypto-js/
+
+Usage
+-----
+
+On its own, this package won't be very helpful. Add a specific crypto package like :
+
+- [`jparker:crypto-aes`](https://github.com/p-j/meteor-crypto-aes)
+
+Other related package : 
+
+- [`jparker:crypto-md5`](https://github.com/p-j/meteor-crypto-md5)
+- [`jparker:crypto-sha1`](https://github.com/p-j/meteor-crypto-sha1)
+- [`jparker:crypto-sha256`](https://github.com/p-j/meteor-crypto-sha256)
+- [`jparker:crypto-base64`](https://github.com/p-j/meteor-crypto-base64)
+- [`jparker:crypto-hmac`](https://github.com/p-j/meteor-crypto-hmac)
+
+See also
+--------
+The CryptoJS project lives at <https://code.google.com/p/crypto-js/> and the documentation for ciphers is at <https://code.google.com/p/crypto-js/#Ciphers>.
--- package.js
+++ package.js
@@ -1,33 +1,35 @@
 Package.describe({
   summary: 'Cipher Core Package for CryptoJS, standard secure algorithms',
-  version: '3.1.2',
+  version: '0.1.0',
   git: 'https://github.com/p-j/meteor-crypto-cipher-core.git'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.1.1');
-  api.use([
-    'jparker:crypto-core@3.1.2',
-  	'jparker:crypto-base64@3.1.2',
-  	'jparker:crypto-evpkdf@3.1.2'
-	], ['client', 'server']);
 
-	api.imply([
-    'jparker:crypto-core',
-    'jparker:crypto-base64',
-  	'jparker:crypto-evpkdf'
+  api.use([
+    'jparker:crypto-core@0.1.0',
+    'jparker:crypto-base64@0.1.0',
+    'jparker:crypto-evpkdf@0.1.0'
   ], ['client', 'server']);
 
-  api.addFiles(['lib/cipher-core.js']);
+  api.imply([
+    'jparker:crypto-core',
+    'jparker:crypto-base64',
+    'jparker:crypto-evpkdf'
+  ], ['client', 'server']);
+
+  api.addFiles('lib/cipher-core.js', ['client', 'server']);
 });
 
 Package.onTest(function (api) {
-	api.use([
-		'jparker:crypto-core@3.1.2',
-		'jparker:crypto-cipher-core@3.1.2',
-		'jparker:crypto-base64@3.1.2',
-		'tinytest'
-	], ['client', 'server']);
+  api.use([
+    'jparker:crypto-core@0.1.0',
+    'jparker:crypto-cipher-core@0.1.0',
+    'jparker:crypto-base64@0.1.0',
+    'jparker:crypto-aes@0.1.0',
+    'tinytest'
+  ], ['client', 'server']);
 
-	api.addFiles('tests/tests.js', ['client', 'server']);
+  api.addFiles('tests/tests.js', ['client', 'server']);
 });
